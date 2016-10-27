010d51dcc231bd1f25b99f09090f33530c5dc463
Stephen Corry <stevemcorry@gmail.com>
2016-10-27 12:03:43 -0600

    done

--- controller.js
+++ controller.js
@@ -1,9 +1,9 @@
 angular.module('userProfiles').controller('MainController', function($scope, mainService) {
   $scope.getUsers = function() {
     mainService.getUsers().then(function(dataFromService) {
-      $scope.users = dataFromService.data.data;
+      $scope.users = dataFromService;
     });
-  }
+  };
 
   $scope.getUsers();
 
